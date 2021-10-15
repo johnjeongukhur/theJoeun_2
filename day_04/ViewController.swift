7e00cbe09637a32e36daac08bb9927293e55dd90
John Jeonguk Hur <johnjeongukhur@gmail.com>
2021-10-15 19:34:40 +0900

    Initial Commit

---
+++ day_04/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  day_04
+//
+//  Created by John Hur on 2021/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
